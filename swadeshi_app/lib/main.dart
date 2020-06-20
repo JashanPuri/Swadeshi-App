759c782896d7fc2f45bd7394fc9e49f5faee1c8c
JashanPuri <jashan2019@iiitkottayam.ac.in>
2020-06-20 16:27:52 +0530

    Font Managed

--- swadeshi_app/lib/main.dart
+++ swadeshi_app/lib/main.dart
@@ -17,6 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+          child: child,
+        );
+      },
       debugShowCheckedModeBanner: false,
       title: 'Swadeshi App',
       theme: ThemeData(
